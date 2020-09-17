6f9f79893fecca1083156f716fed68185ac99ce9
ItisCaleb <40360126+ItisCaleb@users.noreply.github.com>
2020-09-17 16:17:36 +0800

    finish

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ const chromedriver = require('chromedriver');
 const {until,By} = require("selenium-webdriver");
 const Jimp = require('jimp');
 const axios = require('axios');
-const cv = require('opencv')
-
+const spawn = require('child_process').spawn;
 
 chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
 
@@ -17,27 +16,44 @@ const driver = new webdriver.Builder()
     .withCapabilities(webdriver.Capabilities.chrome())
     .build();
 
-async function writeImage(res) {
+ new CronJob('* 2 * * * *',async ()=>{
+        let url = JSON.parse(fs.readFileSync(__dirname+'/url.json','utf8')) ;
+        const idSet = await getUser(url[0])
+        calculatePedoBear(idSet)
+            .then((pedo)=>{
+                let obj={
+                    total:idSet.size,
+                    pedo:pedo
+                }
+                let json = JSON.stringify(obj)
+                fs.writeFileSync(__dirname+'/summary.json',json,'utf8')
+            })
+},null,false).start()
+
+
+
+async function writeImage(total, pedo) {
     const font = await Jimp.loadFont(__dirname + '/font/TaipeiSansTCBeta-Bold.fnt');
     const image = await Jimp.read(__dirname + '/pedobear.jpg');
-    image.print(font, 0, 300, "現在的時間是:");
-    image.print(font, 0, 450, new Date().toISOString()
-        .replace(/T/, ' ')
-        .replace(/\..+/, '')
-    );
-    res.writeHead(200, {'Content-Type': 'image/jpeg'});
-    image.getBuffer(Jimp.MIME_JPEG, (err, value) => {
-        res.end(value, 'binary');
-    })
+    image.print(font, 0, 350, "這串共有"+total+"個巴友");
+    image.print(font, 0, 400, "其中有"+pedo+"個熊頭");
+    if(pedo >= total/3) image.print(font, 0, 450, "FBI正在密切關注這個討論串");
+    else image.print(font, 0, 450, "這個討論串非常正常");
+    return image.getBufferAsync(Jimp.MIME_JPEG)
+        .then(value => {
+            return value;
+        })
+        .catch(err=>{
+          console.log(err)
+        });
 }
-
 async function getCard(id){
     return axios.get(`https://avatar2.bahamut.com.tw/avataruserpic/${id[0]}/${id[1]}/${id}/${id}.png`,
         {
             responseType:'arraybuffer'
         })
         .then((res) => {
-            return Buffer.from(res.data).toString('base64')
+            return Buffer.from(res.data).toString('base64');
         })
         .catch(err=>{
             console.log(err)
@@ -46,7 +62,7 @@ async function getCard(id){
 async function getUser(url){
     try {
         let blacklist = JSON.parse(fs.readFileSync(__dirname+'/blacklist.json','utf8')) ;
-            await driver.get(url[i]);
+            await driver.get(url);
             const pageSource = await driver
                 .wait(until.elementLocated(By.css('body')), 100)
                 .getAttribute('innerHTML');
@@ -54,11 +70,13 @@ async function getUser(url){
             const page = $('.BH-pagebtnA').last().children('a').last().text();
             const idSet = new Set();
             for (let n=1;n<=page;n++){
-                await driver.get(url[i]+`&page=${n}`);
-                const thisPage = await driver
-                    .wait(until.elementLocated(By.css('body')), 100)
-                    .getAttribute('innerHTML');
-                $ = cheerio.load(thisPage);
+                if(n!==1){
+                    await driver.get(url+`&page=${n}`);
+                    const thisPage = await driver
+                        .wait(until.elementLocated(By.css('body')), 100)
+                        .getAttribute('innerHTML');
+                    $ = cheerio.load(thisPage);
+                }
                 const img = $('.gamercard')
                 img.each((i)=>{
                     let userId = img[i].attribs['data-gamercard-userid'].toLowerCase();
@@ -67,20 +85,46 @@ async function getUser(url){
                     }
                 })
             }
-            console.log(idSet)
             return idSet
     }catch (err){
         console.log(err)
     }
 }
+function calculatePedoBear(idSet){
+        return new Promise(async (resolve) => {
+                let pedo=0;
+                let sum=0;
+                for(let [i, user] of idSet.entries()){
+                    const process = spawn('python',['pedo.py'])
+                    process.stdout.on('data',(data)=>{
+                        data = parseInt(data);
+                        if(data>2){
+                            pedo++;
+                        }
+                        sum++
+                        if(sum === idSet.size-1){
+                            resolve(pedo)
+                        }
+                    })
+                    process.stdin.write(await getCard(user))
+                    process.stdin.end()
+                }
+
+
+
+        })
+}
+
 
 app.get('/',async (req, res, next) => {
-        //let url = JSON.parse(fs.readFileSync(__dirname+'/url.json','utf8')) ;
-        //const idSet = getUser(url[0])
-        res.send('<img src=data:image/png;base64,'+await getCard('itiscaleb')+'>');
+     const json = fs.readFileSync(__dirname+'/summary.json','utf-8')
+     const obj = JSON.parse(json)
+     const image = await writeImage(obj.total,obj.pedo)
+     res.end(image,'binary')
+
+
 
 })
-
 
 
 app.listen(3000,()=>{
---
+++ pedo.py
@@ -0,0 +1,53 @@
+import numpy as np
+import cv2
+import sys
+import base64
+
+
+def touint8(image):
+    return np.uint8(np.absolute(image))
+
+
+def show(image):
+    cv2.imshow('test', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def base64_to_cv2(code):
+    nparr = np.frombuffer(base64.b64decode(code), np.uint8)
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    return image
+
+
+head = cv2.imread('pedohead.png', cv2.IMREAD_GRAYSCALE)
+mark = cv2.imread('pedomark.png', cv2.IMREAD_GRAYSCALE)
+user = base64_to_cv2(sys.stdin.read())
+sift = cv2.SIFT_create()
+kp1, des1 = sift.detectAndCompute(head, None)
+kp2, des2 = sift.detectAndCompute(mark, None)
+kp3, des3 = sift.detectAndCompute(user, None)
+
+index_params = dict(algorithm=0, trees=5)
+search_params = dict(checks=50)
+flann = cv2.FlannBasedMatcher(index_params, search_params)
+matches1 = flann.knnMatch(des1, des3, k=2)
+headgood = []
+markgood = []
+for i, (m, n) in enumerate(matches1):
+    if m.distance < 0.7 * n.distance:
+        headgood.append(m)
+
+if len(headgood) == 2:
+    matches2 = flann.knnMatch(des2, des3, k=2)
+    for i, (m, n) in enumerate(matches2):
+        if m.distance < 0.7 * n.distance:
+            markgood.append(m)
+    print(len(markgood))
+    sys.stdout.flush()
+else:
+    print(len(headgood))
+    sys.stdout.flush()
+
+
+
