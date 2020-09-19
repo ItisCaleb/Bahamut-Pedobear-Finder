37aa3086ab4ace5b41ee65473de3bf8997f478e6
ItisCaleb <happycaleb1212@gmail.com>
2020-09-19 15:58:34 +0800

    Update app.js

--- app.js
+++ app.js
@@ -118,9 +118,11 @@ function calculatePedoBear(idSet){
                 let pedo=0;
                 let sum=0;
                 for(let user of idSet){
-                    try{
-                       const process = spawn('python',['pedo.py'])
-                        process.stdout.on('data',(data)=>{
+                    const process = spawn('python',['pedo.py'])
+                    process.stdout.on('error',(err)=>{
+                        console.log(err)
+                    })
+                    process.stdout.on('data',(data)=>{
                         data = parseInt(data);
                         if(data>2){
                             pedo++;
@@ -131,12 +133,8 @@ function calculatePedoBear(idSet){
                             resolve(pedo)
                         }
                     })
-                        process.stdin.write(await getCard(user))
-                        process.stdin.end()
-                    }catch (err){
-                        console.log(err)
-                    }
-
+                    process.stdin.write(await getCard(user))
+                    process.stdin.end()
                 }
 
 
